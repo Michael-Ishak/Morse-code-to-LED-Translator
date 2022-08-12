7535f5e2e44e4de34fcf778e91d571956653729b
Michael Ishak <108626872+Michael-Ishak@users.noreply.github.com>
2022-08-12 20:29:44 +0100

    Add files via upload

---
+++ Morse to LED.ino
@@ -0,0 +1,32 @@
+﻿void setup() {
+	pinMode(13, OUTPUT); //Which pin is changing
+}
+
+void loop() {
+	int i = 0;
+	while (i < str.size())
+	{
+		translated = Morse(toupper(str[i]));
+		if (translated[i] = '-')//If a dash
+		{
+			digitalWrite(13, HIGH); //Turns on LED connecting to pin 13
+			delay(2000); //Keep on for 2000ms
+			digitalWrite(13, LOW);//Turns off LED connecting to pin 13
+			delay(500);//Keep off for 500ms
+		}
+		else if (translated[i] = '.')//if a dot
+		{
+			digitalWrite(13, HIGH); //Turns on LED connecting to pin 13
+			delay(500); //Keep on for 500ms
+			digitalWrite(13, LOW);//Turns off LED connecting to pin 13
+			delay(500);//Keep off for 500ms
+		}
+		else
+		{
+			digitalWrite(13, LOW);//Turns off LED connecting to pin 13
+			delay(2000);//Keep off for 2000ms
+		}
+		i++;
+	}
+	return 0;
+}
